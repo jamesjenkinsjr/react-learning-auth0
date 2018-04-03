ebfcc1acf6f50adbf8b5cab2ba80a670fb6fbfce
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-03 18:49:07 -0400

    Set up router and routes with nav to each component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import Profile from './components/Profile';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div >
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile/123">Profile</Link></li>
+        </ul>
         <Route path='/' exact component={Home} />
         <Route path='/profile/:id' exact component={Profile} />
-        <h1>Respect my authoritah</h1>
       </div>
     );
   }
