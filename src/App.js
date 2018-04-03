123d5f01e57423fbb7b4a26a5a1a17b4afebc698
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-03 18:46:15 -0400

    Add route for home and profile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Home />
-        <Profile />
+        <Route path='/' exact component={Home} />
+        <Route path='/profile/:id' exact component={Profile} />
         <h1>Respect my authoritah</h1>
       </div>
     );
