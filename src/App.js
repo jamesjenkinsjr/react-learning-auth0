39490ccc993647b86aa8838c82274487f90eefba
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-03 18:39:09 -0400

    Add Profile component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import Profile from './components/Profile';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div >
         <Home />
+        <Profile />
         <h1>Respect my authoritah</h1>
       </div>
     );
---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = props => (
+
+    <div>
+        <h1>This is your profile</h1>
+    </div>
+);
+
+export default Profile;
