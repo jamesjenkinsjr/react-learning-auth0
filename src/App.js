d35b232bf1a730c1f0f7221e3eb2f436a61b1c4b
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-03 18:36:58 -0400

    Create Home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Home from './components/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div >
+        <Home />
         <h1>Respect my authoritah</h1>
       </div>
     );
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = props => (
+    <div>
+        <h1>this is your home.</h1>
+    </div>
+);
+
+export default Home;
