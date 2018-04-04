4f4d81b6aedc0167f5becccc657524ffa3054e51
James Jenkins <jamesjenkinsjr@icloud.com>
2018-04-03 20:46:35 -0400

    Create callback and wire up authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import Profile from './components/Profile';
+import Callback from './components/Callback';
 import { Route, Link } from 'react-router-dom';
 import Auth from './services/auth';
 import './App.css';
@@ -11,6 +12,11 @@ class App extends Component {
     this.auth = new Auth();
   }
   
+  handleAuthentication = (nextState, replace) => {
+    if (/access_token|id_token|error/.test(nextState.location.hash)) {
+      this.auth.handleAuthentication();
+    }
+  }
   render() {
     return (
       <div >
@@ -21,6 +27,10 @@ class App extends Component {
         </ul>
         <Route path='/' exact component={Home} />
         <Route path='/profile/:id' exact component={Profile} />
+        <Route path='/callback' exact render={(props) => {
+          this.handleAuthentication(props);
+          return <Callback {...props} />
+        }} />
       </div>
     );
   }
---
+++ src/components/Callback.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Callback = props => (
+    <div>I ain't no Call-a-back route</div>
+);
+
+export default Callback;
